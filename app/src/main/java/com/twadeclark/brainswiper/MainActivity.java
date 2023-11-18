a211058cbe9b4f3a1829e8e2810bc1319b004f87
twadeclark <115502590+twadeclark@users.noreply.github.com>
2023-11-17 22:45:09 -0800

    updated android studio

--- app/src/main/java/com/twadeclark/brainswiper/MainActivity.java
+++ app/src/main/java/com/twadeclark/brainswiper/MainActivity.java
@@ -2,7 +2,9 @@ package com.twadeclark.brainswiper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void createNewDeck(View view) {
+//        Intent intent = new Intent(this, NewDeckActivity.class);
+        Intent intent = new Intent(this, DeckEditorFragment.class);
+        startActivity(intent);
+    }
+
+
 }
---
+++ app/src/main/java/com/twadeclark/brainswiper/NewDeckActivity.java
@@ -0,0 +1,13 @@
+package com.twadeclark.brainswiper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewDeckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_deck);
+    }
+}
